c5ee06e375b60e618447ece6d1376afcff9c9315
simeonrad <simeonrad@abv.bg>
2023-11-13 13:54:27 +0200

    Minor changes in the Change commands.

--- src/commands/ChangeBugPriorityCommand.java
+++ src/commands/ChangeBugPriorityCommand.java
@@ -5,6 +5,8 @@ import core.contracts.Repository;
 import models.contracts.Bug;
 import models.contracts.Task;
 import models.enums.Priority;
+import utils.ParsingHelpers;
+import utils.ValidationHelpers;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -12,19 +14,20 @@ import java.util.List;
 public class ChangeBugPriorityCommand implements Command {
 
     private final Repository repository;
-    private static final int BUG_ID_INDEX = 1;
-    private static final int NEW_PRIORITY_INDEX = 2;
-    private List<Task> tasks;
+    private static final int BUG_ID_INDEX = 0;
+    private static final int NEW_PRIORITY_INDEX = 1;
+    private static final int EXPECTED_ARGUMENTS = 2;
 
     public ChangeBugPriorityCommand(Repository repository) {
         this.repository = repository;
-        this.tasks = new ArrayList<>(tasks);
     }
 
     @Override
     public String execute(List<String> parameters) {
+        ValidationHelpers.validateArgumentsCount(parameters, EXPECTED_ARGUMENTS);
+
         //bugId
-        int bugId = Integer.parseInt(parameters.get(BUG_ID_INDEX));
+        int bugId = ParsingHelpers.tryParseInteger(parameters.get(BUG_ID_INDEX), "Bug ID");
 
         //newPriority
         Priority newPriority = Priority.valueOf(parameters.get(NEW_PRIORITY_INDEX));

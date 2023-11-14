f4feae01f1da9185851e8704498be456d939d1fd
simeonrad <simeonrad@abv.bg>
2023-11-14 18:28:27 +0200

    ListingHelpers class created

---
+++ src/utils/ListingHelpers.java
@@ -0,0 +1,29 @@
+package utils;
+
+import models.contracts.Person;
+import models.contracts.Team;
+import java.util.ArrayList;
+import java.util.List;
+
+public class ListingHelpers {
+
+    public static <T extends Person> String peopleToString(List<T> elements) {
+        List<String> result = new ArrayList<>();
+        for (T element : elements) {
+            result.add(element.getName());
+        }
+        result.sort(String::compareToIgnoreCase);
+        return String.format("People: " + String.join(", ", result).trim());
+    }
+
+    public static <T extends Team> String teamsToString(List<T> elements) {
+        List<String> result = new ArrayList<>();
+        for (T element : elements) {
+            result.add(element.getName());
+        }
+        result.sort(String::compareToIgnoreCase);
+        return String.format("Teams: " + String.join(", ", result).trim());
+    }
+
+
+}

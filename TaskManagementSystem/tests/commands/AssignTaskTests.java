b903050b39e9d17e0457927740a1022bdb9b9726
DanailRusev <138879093+DanailRusev@users.noreply.github.com>
2023-11-20 11:49:46 +0200

    Added Assign and Unassign Tests

---
+++ TaskManagementSystem/tests/commands/AssignTaskTests.java
@@ -0,0 +1,77 @@
+package commands;
+
+import Utils.TestUtilities;
+import core.RepositoryImpl;
+import core.contracts.Repository;
+import exceptions.NoSuchElementFoundException;
+import models.FeedbackImpl;
+import models.PersonImpl;
+import models.contracts.Feedback;
+import models.contracts.Person;
+import models.contracts.Story;
+import models.enums.Priority;
+import models.enums.Size;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+public class AssignTaskTests {
+    private final static int EXPECTED_PARAMETERS_COUNT = 2;
+    public static final String VALID_NAME = TestUtilities.getString(6);
+    public static final String VALID_TITLE = TestUtilities.getString(11);
+    public static final String VALID_DESCRIPTION = TestUtilities.getString(11);
+
+    private Repository repository;
+    private AssignTaskCommand command;
+
+    @BeforeEach
+    public void setUp() {
+        repository = new RepositoryImpl();
+        command = new AssignTaskCommand(repository);
+    }
+    @Test
+    public void execute_Should_Throw_When_ParametersCountInvalid(){
+        List<String> parameters = TestUtilities.getList(EXPECTED_PARAMETERS_COUNT +1);
+        Assertions.assertThrows(IllegalArgumentException.class, ()-> command.execute(parameters));
+    }
+    @Test
+    public void execute_Should_Throw_When_TaskDoesNotExists(){
+        new PersonImpl(VALID_NAME);
+        List<String> parameters = List.of("1",VALID_NAME);
+        Assertions.assertThrows(NoSuchElementFoundException.class, ()-> command.execute(parameters));
+    }
+    @Test
+    public void execute_Should_Throw_When_PersonDoesNotExists(){
+        new FeedbackImpl(1,VALID_TITLE,VALID_DESCRIPTION,2);
+        List<String> parameters = List.of("1",VALID_NAME);
+        Assertions.assertThrows(NoSuchElementFoundException.class, ()-> command.execute(parameters));
+    }
+    @Test
+    public void execute_Should_Throw_When_TaskIsFeedback(){
+        repository.createPerson(VALID_NAME);
+        Feedback feedback = repository.createFeedback(VALID_TITLE,VALID_DESCRIPTION,2);
+        List<String> parameters = List.of(String.valueOf(feedback.getId()),VALID_NAME);
+        Assertions.assertThrows(IllegalArgumentException.class, ()-> command.execute(parameters));
+    }
+    @Test
+    public void execute_Should_AddTaskToAssignee_When_ArgumentsAreValid(){
+        Person person = repository.createPerson(VALID_NAME);
+        Story story = repository.createStory(VALID_TITLE, VALID_DESCRIPTION, Priority.LOW, Size.SMALL,person);
+        List<String> parameters = List.of(String.valueOf(story.getId()),VALID_NAME);
+        command.execute(parameters);
+        Assertions.assertEquals(1,person.getTasks().size());
+    }
+    @Test
+    public void execute_Should_AddAssigneeToTask_When_ArgumentsAreValid(){
+        Person person = repository.createPerson(VALID_NAME);
+        Story story = repository.createStory(VALID_TITLE, VALID_DESCRIPTION, Priority.LOW, Size.SMALL,person);
+        List<String> parameters = List.of(String.valueOf(story.getId()),VALID_NAME);
+        story.unassign();
+        command.execute(parameters);
+        Assertions.assertEquals(person.getName(),story.getAssignee().getName());
+    }
+
+}
+
---
+++ TaskManagementSystem/tests/commands/UnassignTaskTests.java
@@ -0,0 +1,69 @@
+package commands;
+
+import Utils.TestUtilities;
+import core.RepositoryImpl;
+import core.contracts.Repository;
+import exceptions.NoSuchElementFoundException;
+import models.PersonImpl;
+import models.contracts.Feedback;
+import models.contracts.Person;
+import models.contracts.Story;
+import models.enums.Priority;
+import models.enums.Size;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+public class UnassignTaskTests {
+    private final static int EXPECTED_PARAMETERS_COUNT = 1;
+    public static final String VALID_NAME = TestUtilities.getString(6);
+    public static final String VALID_TITLE = TestUtilities.getString(11);
+    public static final String VALID_DESCRIPTION = TestUtilities.getString(11);
+
+    private Repository repository;
+    private UnassignTaskCommand command;
+
+    @BeforeEach
+    public void setUp() {
+        repository = new RepositoryImpl();
+        command = new UnassignTaskCommand(repository);
+    }
+    @Test
+    public void execute_Should_Throw_When_ParametersCountInvalid(){
+        List<String> parameters = TestUtilities.getList(EXPECTED_PARAMETERS_COUNT +1);
+        Assertions.assertThrows(IllegalArgumentException.class, ()-> command.execute(parameters));
+    }
+    @Test
+    public void execute_Should_Throw_When_TaskDoesNotExists(){
+        new PersonImpl(VALID_NAME);
+        List<String> parameters = List.of("1");
+        Assertions.assertThrows(NoSuchElementFoundException.class, ()-> command.execute(parameters));
+    }
+    @Test
+    public void execute_Should_Throw_When_TaskIsFeedback(){
+        repository.createPerson(VALID_NAME);
+        Feedback feedback = repository.createFeedback(VALID_TITLE,VALID_DESCRIPTION,2);
+        List<String> parameters = List.of(String.valueOf(feedback.getId()));
+        Assertions.assertThrows(IllegalArgumentException.class, ()-> command.execute(parameters));
+    }
+    @Test
+    public void execute_Should_RemoveTaskFromAssignee_When_ArgumentsAreValid(){
+        Person person = repository.createPerson(VALID_NAME);
+        Story story = repository.createStory(VALID_TITLE, VALID_DESCRIPTION, Priority.LOW, Size.SMALL,person);
+        List<String> parameters = List.of(String.valueOf(story.getId()));
+        person.addTask(story);
+        command.execute(parameters);
+        Assertions.assertEquals(0,person.getTasks().size());
+    }
+    @Test
+    public void execute_Should_RemoveAssigneeFromTask_When_ArgumentsAreValid(){
+        Person person = repository.createPerson(VALID_NAME);
+        Story story = repository.createStory(VALID_TITLE, VALID_DESCRIPTION, Priority.LOW, Size.SMALL,person);
+        List<String> parameters = List.of(String.valueOf(story.getId()));
+        person.addTask(story);
+        command.execute(parameters);
+        Assertions.assertNotEquals(person,story.getAssignee());
+    }
+}
